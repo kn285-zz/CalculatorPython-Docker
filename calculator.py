ff259fbe2048bc508fc9ae1bd9dc76ba93f0179c
kn285 <55518570+kn285@users.noreply.github.com>
2020-02-02 12:16:27 -0500

    Feature: Added subtraction, multiplication and division

--- calculator.py
+++ calculator.py
@@ -5,3 +5,9 @@ class Calculator:
 
     def addition(self, a, b):
         return float(a) + float(b)
+    def subtraction(self, a, b):
+        return float(a) - float(b)
+    def multiplication(self, a, b):
+        return float(a) * float(b)
+    def division(self, a, b):
+        return float(b) / float(a)
